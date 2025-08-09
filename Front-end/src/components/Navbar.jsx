cc6cdb905d196de7f023aea8debfcbb48946230b
Krrishbalana <krrishbalana@gmail.com>
2025-08-10 00:37:32 +0530

    setup basic frontend app, installed all dependencies, setup routes

---
+++ Front-end/src/components/Navbar.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <div className='flex items-center justify-center py-5 font-medium'>
+           hello
+    </div>
+  )
+}
+
+export default Navbar
